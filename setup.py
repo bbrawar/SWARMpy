20d05d6f92e06ad18288b41c0ba2727ca35909c2
Bhuvnesh Brawar <90771976+bbrawar@users.noreply.github.com>
2025-01-18 19:21:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 
-# pySWARM setup configuration
+# SWARMpy setup configuration
 setup(
-    name="pySWARM",
+    name="SWARMpy",
     version="1.0.0",
     description="A Python module for loading .CDF files into pandas or xarray data structures.",
     long_description="""
-pySWARM is a lightweight Python package that facilitates the loading of .CDF files into either pandas DataFrames or xarray Datasets.
+SWARMpy is a lightweight Python package that facilitates the loading of .CDF files into either pandas DataFrames or xarray Datasets.
 It leverages SpacePy for .CDF file reading and provides a simple interface to convert and work with these files efficiently.
     """,
     long_description_content_type="text/markdown",
     author="Bhuvnesh Brawar",
     author_email="bbrawar@gmail.com",
-    url="https://github.com/bbrawar/pySWARM",
+    url="https://github.com/bbrawar/SWARMpy",
     license="MIT",
     packages=find_packages(),
     install_requires=[
@@ -33,8 +33,8 @@ It leverages SpacePy for .CDF file reading and provides a simple interface to co
     python_requires=">=3.7",
     keywords="cdf pandas xarray spacepy",
     project_urls={
-        "Bug Reports": "https://github.com/yourusername/pyswarm/issues",
-        "Source": "https://github.com/yourusername/pyswarm",
+        "Bug Reports": "https://github.com/bbrawar/SWARMpy/issues",
+        "Source": "https://github.com/bbrawar/SWARMpy",
     },
 )
 
